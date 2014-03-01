a13a348f518382e66ca80f5a2e9bf96773284027
James Young <pronoiac@gmail.com>
2014-02-28 20:45:21 -0800

    Cleaning up prune script - removing unused variables.

--- tarsnap-prune.sh
+++ tarsnap-prune.sh
@@ -12,20 +12,10 @@ CONFIG=/etc/tarsnap-cron.conf
 # etc 	/etc
 # www 	/var/www
 
-# Number of daily backups to keep
+# Number of daily / weekly / monthly backups to keep
 DAILY=7
-
-# Number of weekly backups to keep
 WEEKLY=5
-# Which day to do weekly backups on
-# 1-7, Monday = 1
-WEEKLY_DAY=1
-
-# Number of monthly backups to keep
 MONTHLY=12
-# Which day to do monthly backups on
-# 01-31 (leading 0 is important)
-MONTHLY_DAY=01
 
 # Path to tarsnap
 #TARSNAP="/home/tdb/tarsnap/tarsnap.pl"
